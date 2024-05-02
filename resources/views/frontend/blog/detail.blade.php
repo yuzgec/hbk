97fefa2aac10066b46a7d48f4ae5f21c93991179
yuzgec <olcayy@gmail.com>
2024-05-02 14:46:05 +0300

    Fix

--- resources/views/frontend/blog/detail.blade.php
+++ resources/views/frontend/blog/detail.blade.php
@@ -1,24 +1,23 @@
 @extends('frontend.layout.app')
 @section('content')
 
-    <section class="page-header page-header-modern bg-color-quaternary page-header-md custom-page-header">
-        <section class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-dark overlay-show overlay-op-7" style="background-image: url(img/page-header/page-header-services.jpg);">
-            <div class="container">
-                <div class="row mt-2">
-                    <div class="col-md-12 align-self-center p-static order-2 text-center">
-                        <h1 class="text-10">İstanbul {{ $Detail->title }}</strong></h1>
-                        <span class="sub-title">Kepenk Tamiri ve Montajı</span>
-                    </div>
-                    <div class="col-md-12 align-self-center order-1">
-                        <ul class="breadcrumb breadcrumb-light d-block text-center">
-                            <li><a href="{{ route('home')}}">Anasayfa</a></li>
-                            <li><a href="#">Haberler</a></li>
-                            <li class="active">{{ $Detail->title }}</li>
-                        </ul>
-                    </div>
+    <section class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-dark overlay-show overlay-op-7" style="background-image: url(img/page-header/page-header-services.jpg);">
+        <div class="container">
+            <div class="row mt-2">
+                <div class="col-md-12 align-self-center p-static order-2 text-center">
+                    <h1 class="text-10">İstanbul {{ $Detail->title }}</strong></h1>
+                    <span class="sub-title">Kepenk Tamiri ve Montajı</span>
+                </div>
+                <div class="col-md-12 align-self-center order-1">
+                    <ul class="breadcrumb breadcrumb-light d-block text-center">
+                        <li><a href="{{ route('home')}}">Anasayfa</a></li>
+                        <li><a href="#">Haberler</a></li>
+                        <li class="active">{{ $Detail->title }}</li>
+                    </ul>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
     <div class="container">
         <div class="row pb-4">
